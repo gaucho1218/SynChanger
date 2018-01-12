5136b5b9c6ff2e73a8cb505e8295676ed47684c9
gaucho1218 <gaucho1218@gmail.com>
2018-01-13 00:42:34 +0900

    add swift sample for studying swift

---
+++ swiftSample/SynChangerSwiftSample/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SynChangerSwiftSample
+//
+//  Created by Hansuck Shim on 2018. 1. 13..
+//  Copyright © 2018년 gaucho1218. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
