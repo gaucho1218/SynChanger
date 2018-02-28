ecfcd1655a4ca91677572ec60515e6827f75624c
gaucho1218 <gaucho1218@gmail.com>
2018-02-28 17:41:44 +0900

    add rich text box and button for change syntax (will work on other branch)

--- csharp/SynChanger/Form1.Designer.cs
+++ csharp/SynChanger/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SynChanger
 {
-    partial class Form1
+    partial class mainForm
     {
         /// <summary>
         /// 필수 디자이너 변수입니다.
@@ -28,12 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.beforeTextBox = new System.Windows.Forms.RichTextBox();
+            this.afterTextBox = new System.Windows.Forms.RichTextBox();
+            this.changeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // beforeTextBox
+            // 
+            this.beforeTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.beforeTextBox.Location = new System.Drawing.Point(12, 59);
+            this.beforeTextBox.Name = "beforeTextBox";
+            this.beforeTextBox.Size = new System.Drawing.Size(250, 290);
+            this.beforeTextBox.TabIndex = 2;
+            this.beforeTextBox.Text = "";
+            // 
+            // afterTextBox
+            // 
+            this.afterTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.afterTextBox.Location = new System.Drawing.Point(322, 59);
+            this.afterTextBox.Name = "afterTextBox";
+            this.afterTextBox.Size = new System.Drawing.Size(250, 290);
+            this.afterTextBox.TabIndex = 3;
+            this.afterTextBox.Text = "";
+            // 
+            // changeButton
+            // 
+            this.changeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.changeButton.Location = new System.Drawing.Point(497, 12);
+            this.changeButton.Name = "changeButton";
+            this.changeButton.Size = new System.Drawing.Size(75, 23);
+            this.changeButton.TabIndex = 4;
+            this.changeButton.Text = "Change";
+            this.changeButton.UseVisualStyleBackColor = true;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.changeButton);
+            this.Controls.Add(this.afterTextBox);
+            this.Controls.Add(this.beforeTextBox);
+            this.Name = "mainForm";
+            this.Text = "SynChanger";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox beforeTextBox;
+        private System.Windows.Forms.RichTextBox afterTextBox;
+        private System.Windows.Forms.Button changeButton;
     }
 }
 
