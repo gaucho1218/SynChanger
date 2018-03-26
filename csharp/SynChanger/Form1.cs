bafb31c4043a042286d221d1d5d48b4f42850bf1
gaucho1218 <gaucho1218@gmail.com>
2018-03-26 18:27:12 +0900

    #14 done

--- csharp/SynChanger/Form1.cs
+++ csharp/SynChanger/Form1.cs
@@ -56,6 +56,7 @@ namespace SynChanger
         {
             changeSyntax();
             copyToClipBoard();
+            MessageBox.Show("Result copied to clipboard!");
         }
 
         private void mainForm_Resize(object sender, EventArgs e)
